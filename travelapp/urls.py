c45a94fff4f9ec698e8808c6a0f77a2c2b6d4158
arjun premji <arjunpremji@arjuns-MacBook-Air.local>
2025-02-22 14:33:34 +0530

    first commit

--- travelapp/urls.py
+++ travelapp/urls.py
@@ -3,8 +3,16 @@ from . import views
 
 urlpatterns = [
     path('index/', views.index, name='index'),
-]
-
-urlpatterns = [
     path('about/', views.about, name='about'),
+    path('fyp/', views.fyp, name='fyp'),
+    path('service/', views.service, name='service'),
+    path('login/', views.login, name='login'),
+    path('recomendations/', views.recomendations, name='recomendations'),
+    path('contact/', views.contact, name='contact'),
+    path('userdash/', views.userdash, name='userdash'),
+    path('favlocation/', views.favlocation, name='favlocation'),
+    path('recentrecom/', views.recentrecom, name='recentrecom'),
+    path('support/', views.support, name='support'),
+    path('testimonial/', views.testimonial, name='testimonial'),
+    path('profile/', views.profile, name='profile'),
 ]
--- travelapp/views.py
+++ travelapp/views.py
@@ -8,4 +8,35 @@ def index(request):
 def about(request):
     return render(request,'about.html')
 
+def fyp(request):
+    return render(request,'fyp.html')
 
+def service(request):
+    return render(request,'service.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def recomendations(request):
+    return render(request,'recomendations.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def userdash(request):
+    return render(request,'userdash.html')
+
+def favlocation(request):
+    return render(request,'favlocation.html')
+
+def recentrecom(request):
+    return render(request,'recentrecom.html')
+
+def support(request):
+    return render(request,'support.html')
+
+def testimonial(request):
+    return render(request,'testimonial.html')
+
+def profile(request):
+    return render(request,'profile.html')
